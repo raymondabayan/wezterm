87d31077ec5e9ecf63ff5ee43c0090ba190d2dfc
raymondabayan <raymond-john_abayan@bio-rad.com>
2022-07-15 00:00:38 -0700

    added tomorrow theme

--- wezterm.lua
+++ wezterm.lua
@@ -1,17 +1,30 @@
 -- Initial requirement of wezterm variables
 local wezterm = require 'wezterm';
+-- local catppuccin = require("colors/catppuccin").setup {
+--   sync = true,
+--   sync_flavours = {
+--     light = "macchiato",
+--     dark = "mocha"
+--   },
+--   flavour = "mocha"
+-- }
 
 -- Config region
 return {
   -- Specify font
   font = wezterm.font("Hack Nerd Font"),
   font_size = 18,
-  color_scheme = "Dracula",
+  -- color_scheme = "Dracula",
+  -- colors = catppuccin,
+  -- color_scheme = "zenbones_light",
+  color_scheme = "Tomorrow",
+  -- color_scheme = "Github",
   -- Window options
   -- opacity
-  window_background_opacity = 0.93,
+  window_background_opacity = 0.94,
   -- disable tab bar (true be default)
   enable_tab_bar = false,
+  enable_scrollbar = false,
   -- Decorations
   window_decorations = "RESIZE", -- no frame
   -- Visual bell flash for errors
@@ -23,5 +36,11 @@ return {
   },
   colors = {
     visual_bell = "#ff6961"
+  },
+  window_padding = {
+    left = "0.5cell",
+    right = "0cell",
+    top = "0.2cell",
+    bottom = "0cell",
   },
 }
